cffa755a211bf70ae3a3b7fb64d339a0c6aa4bb5
CarstenS <starke.carsten@gmx.de>
2024-10-01 14:22:53 +0200

    Initial Commit

---
+++ MapKitGpsProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapKitGpsProject
+//
+//  Created by Carsten Starke on 01.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
